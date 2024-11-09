8c46b85da9b3587383ae78de70a12e33e94e2788
boodyf2 <abdalrahman.vim@gmail.com>
2024-11-09 22:31:58 +0200

    reusable comps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
 import type { Metadata } from "next";
+import { Inter, EB_Garamond } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
     title: "PingPanda",
     description: "Saas",
 };
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const eb_garamond = EB_Garamond({
+    subsets: ["latin"],
+    variable: "--font-heading",
+});
 
 const RootLayout = ({
     children,
@@ -12,8 +20,10 @@ const RootLayout = ({
     children: React.ReactNode;
 }>) => {
     return (
-        <html lang="en">
-            <body className="antialiased">{children}</body>
+        <html className={cn(inter.variable, eb_garamond.variable)} lang="en">
+            <body className="font-sans antialiased bg-brand-50 text-brand-950">
+                {children}
+            </body>
         </html>
     );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
+import Heading from "@/components/Heading";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <>
+            <section className="relative py-24 sm:py-32 bg-brand-25">
+                <MaxWidthWrapper className="text-center">
+                    <div className="relative mx-auto text-center flex flex-col items-center gap-10">
+                        <Heading>Real-Time SaaS Insights,</Heading>
+                    </div>
+                </MaxWidthWrapper>
+            </section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+        </>
+    );
 };
 
 export default Home;
